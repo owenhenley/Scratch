bcfdd996033de5c6ff327fa83378c30d3a53ee6f
Owen Henley <owenhenley@me.com>
2018-09-20 18:35:12 -0600

    fetch from CK added

--- Scratch/ScratchController.swift
+++ Scratch/ScratchController.swift
@@ -21,8 +21,30 @@ class ScratchController {
     let privateDatabase = CKContainer.default().privateCloudDatabase
     let sharedDatabase = CKContainer.default().sharedCloudDatabase
     
-    func fetchFromCK() {
+    func fetchFromCK(completion: @escaping (Bool) -> Void) {
         
+        let predicate = NSPredicate(value: true)
+        let query = CKQuery(recordType: Scratch.typeKey, predicate: predicate)
+        
+        privateDatabase.perform(query, inZoneWith: nil) { (records, error) in
+            if let error = error {
+                print("Error fetching from icloud: \(error.localizedDescription)")
+                completion(false)
+                return
+            }
+            
+            guard let records = records else {
+                completion(false)
+                return
+            }
+            var scratches: [Scratch] = []
+            for record in records {
+                guard let loadedScratch = Scratch(ckRecord: record) else { continue }
+                scratches.append(loadedScratch)
+            }
+            self.entries = scratches
+            completion(true)
+        }
     }
     
     func saveToCK() {
